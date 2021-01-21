91af449e023f9f61cf3f428dd461c5fff78f6879
lcs <cslee@itmoulds.com>
2021-01-21 14:43:01 +0900

    JSX 인스턴스화(태깅) 한것을 렌더함수 매개변수로 사용

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDom from 'react-dom';
 // HTML 생성하는 새로운 컴포넌트를 만든다
 // some HTML
@@ -5,4 +6,5 @@ const App = function() {
   return <div>Hi!</div>;
 };
 // 이 컴포넌트가 만든 HTML 을 갖고 페이지에 반영한다.
-ReactDom.render(App);
+// 렌더 함수는 인스턴스화된 컴포넌트를 받는다. JSX의 인스턴스화는 태깅으로
+ReactDom.render(<App/>);
