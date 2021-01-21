2de7a7f16cfdfb4f2aaba01ae5047a8c9348481f
lcs <cslee@itmoulds.com>
2021-01-21 20:33:50 +0900

    앱에서 비디오 데이터 가져오기

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import YTsearch from 'youtube-api-search';
 import SETTINGS from '../secret';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY = SETTINGS.API_KEY;
 
 // HTML 생성하는 새로운 컴포넌트를 만든다
 // some HTML
-const App = () => {
-  return (
-      <div>
-        <SearchBar />
-      </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+
+    YTsearch({
+      key: API_KEY,
+      term: '메이플',
+    }, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <SearchBar />
+        </div>
+    );
+  }
+}
+
 // 이 컴포넌트가 만든 HTML 을 갖고 페이지에 반영한다.
 // 렌더 함수는 인스턴스화된 컴포넌트를 받는다. JSX의 인스턴스화는 태깅으로
 // 두번째 인자로 매핑할 노드를 알려준다.
