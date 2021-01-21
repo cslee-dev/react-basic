d039cdf16519b590d8dc07ca0ad0ebd64859f573
lcs <cslee@itmoulds.com>
2021-01-21 15:20:15 +0900

    유튜브 API KEY를 담은 시크릿 스크립트 생성 및 적용 깃 추가 무시 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SETTINGS from '../secret';
+
+const API_KEY = SETTINGS.API_KEY;
+
 // HTML 생성하는 새로운 컴포넌트를 만든다
 // some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return <div>HIHI</div>;
 };
 // 이 컴포넌트가 만든 HTML 을 갖고 페이지에 반영한다.
 // 렌더 함수는 인스턴스화된 컴포넌트를 받는다. JSX의 인스턴스화는 태깅으로
