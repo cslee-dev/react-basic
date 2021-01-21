63d5f6384c74cbbc8d0b6625b611b6f001f156ee
lcs <cslee@itmoulds.com>
2021-01-21 16:30:56 +0900

    서치파 상태 초기화 하기

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,6 +4,11 @@ import React, {Component} from 'react';
 //   return <input />;
 // };
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+  }
+
   render() {
     return (
         <input onChange={event => console.log(event.target.value)} />
