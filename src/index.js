fe18c70cd788cebd45a0bf18638b7e4e9ae8fcea
lcs <cslee@itmoulds.com>
2021-01-21 14:36:39 +0900

    React를 Dom에 렌더링하기 위해서 ReactDom 라이브러리 사용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import ReactDom from 'react-dom';
 // HTML 생성하는 새로운 컴포넌트를 만든다
 // some HTML
 const App = function() {
   return <div>Hi!</div>;
 };
 // 이 컴포넌트가 만든 HTML 을 갖고 페이지에 반영한다.
-React.render(App);
+ReactDom.render(App);
