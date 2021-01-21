e8c6de2f77894cac59ac2568df5f28197f298634
lcs <cslee@itmoulds.com>
2021-01-22 08:13:10 +0900

    비디오 리스트 아이템 데이터 전달 및 스타일링

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import VideoListItem from './video_list_item';
 
 const VideoList = ({videos}) => {
   const videoItems = videos.map(video=>{
-    return <VideoListItem video={video.snippet.channelTitle} key={video.id.videoId}/>
+    return <VideoListItem video={video} key={video.id.videoId}/>
   })
   return (
       <ul className="col-md-4 list-group">
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => {
-  return <li>{video}</li>;
+  const IMAGE_URL = video.snippet.thumbnails.default.url;
+  const VIDEO_TITLE = video.snippet.title;
+  return (
+      <li className="list-group-item">
+        <div className="video-list media">
+          <div className="media-left">
+            <img
+                className="media-object"
+                src={IMAGE_URL}
+            />
+          </div>
+          <div className="media-body">
+            <div className="media-heading">
+              {VIDEO_TITLE}
+            </div>
+          </div>
+        </div>
+      </li>
+  );
 };
 
 export default VideoListItem;
