357ab867ba5c878ea477e77dee00ddba35411f96
lcs <cslee@itmoulds.com>
2021-01-21 14:45:16 +0900

    두번쨰 인자로 매핑할 노드 알려줌

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ const App = function() {
 };
 // 이 컴포넌트가 만든 HTML 을 갖고 페이지에 반영한다.
 // 렌더 함수는 인스턴스화된 컴포넌트를 받는다. JSX의 인스턴스화는 태깅으로
-ReactDom.render(<App/>);
+// 두번째 인자로 매핑할 노드를 알려준다.
+ReactDom.render(<App/>, document.querySelector('.container'));
